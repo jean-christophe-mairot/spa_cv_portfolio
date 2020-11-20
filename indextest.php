1a9ee43778d94ee60c670a2e5e52b8db556835d2
jc mairot <j.mairot@codeur.online>
2020-11-20 08:37:52 +0100

    fin de la structure du portfolio

--- indextest.php
+++ indextest.php
@@ -1,11 +1,242 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jean christophe Mairot</title>
-</head>
-<body>
-    hea
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@400;700&family=Oranienbaum&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/variables.css" />
+    <title>Jean Christophe Mairot</title>
+  </head>
+  <body>
+    <!-- mise en place du header -->
+    <div class="grid-container">
+      <header>
+        <h1><span class="rot-jean">Jean Christophe Mairot</span></h1>
+        <p>Développeur</p>
+        <br />
+        <p>Web Front end | Back end</p>
+      </header>
+      <!-- médaillon avec la photo -->
+      <section class="picture">
+        <div class="photoId1">
+          <img src="img/photo_id_ideas.jpg" alt="photo id design" />
+        </div>
+      </section>
+      <aside>
+        <div class="block">
+          <h2 class="title-small">CONTACT</h2>
+          <h3>Téléphone</h3>
+          <p>06 25 80 06 13</p>
+          <h3>Email</h3>
+          <p>jcmairot@gmail.com</p>
+          <h3>Adresse</h3>
+          <p>
+            2 chemin de la porte de paris <br />77130 - Montereau Fault Yonne
+          </p>
+        </div>
+
+        <!-- skill dev -->
+        <div class="block">
+          <h2 class="title-small">DEV SKILLS</h2>
+          <h3>HTML</h3>
+          <div class="bar-skill">
+            <div class="bar-skill-progress"></div>
+            <div class="bar-skill-dot"></div>
+          </div>
+          <h3>CSS / SCSS</h3>
+          <div class="bar-skill">
+            <div class="bar-skill-progress"></div>
+            <div class="bar-skill-dot"></div>
+          </div>
+          <h3>PHP</h3>
+          <div class="bar-skill">
+            <div class="bar-skill-progress-php"></div>
+            <div class="bar-skill-dot-php"></div>
+          </div>
+          <h3>JS</h3>
+          <div class="bar-skill">
+            <div class="bar-skill-progress-js"></div>
+            <div class="bar-skill-dot-js"></div>
+          </div>
+        </div>
+        <!-- skill graph -->
+        <div class="block">
+          <h2 class="title-small">GRAPH SKILLS</h2>
+          <h3>PHOTOSHOP</h3>
+          <div class="bar-skill">
+            <div class="bar-skill-progress"></div>
+            <div class="bar-skill-dot"></div>
+          </div>
+          <h3>AFTER EFFECT</h3>
+          <div class="bar-skill">
+            <div class="bar-skill-progress-ae"></div>
+            <div class="bar-skill-dot-ae"></div>
+          </div>
+          <h3>NUKE</h3>
+          <div class="bar-skill">
+            <div class="bar-skill-progress-nuke"></div>
+            <div class="bar-skill-dot-nuke"></div>
+          </div>
+          <h3>REVIVAL</h3>
+          <div class="bar-skill">
+            <div class="bar-skill-progress-revi"></div>
+            <div class="bar-skill-dot-revi"></div>
+          </div>
+        </div>
+        <!-- langue -->
+        <div class="block">
+          <h2 class="title-small">LANGUES</h2>
+          <h3>FRAN&#199;AIS</h3>
+          <div class="stars-container">
+            <i class="fas fa-star text-primary"></i>
+            <i class="fas fa-star text-primary"></i>
+            <i class="fas fa-star text-primary"></i>
+            <i class="fas fa-star text-primary"></i>
+            <i class="fas fa-star text-primary"></i>
+          </div>
+          <h3>ANGLAIS TECHNIQUE</h3>
+          <div class="stars-container">
+            <i class="fas fa-star text-primary"></i>
+            <i class="fas fa-star text-primary"></i>
+            <i class="fas fa-star text-primary"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+          </div>
+          <!-- social -->
+          <div class="block">
+            <h2 class="title-small">SOCIAL</h2>
+            <div class="social-container">
+              <i class="fab fa-linkedin"></i>
+              <p>mairot-jc-vfx-web</p>
+            </div>
+            <div class="social-container">
+              <i class="fab fa-github-square"></i>
+              <p>jean-christophe-mairot</p>
+            </div>
+          </div>
+        </div>
+      </aside>
+      <main>
+        <h2 class="title-small">PROFIL PROFESSIONNEL</h2>
+        <p class="mb-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          commodi aut quidem nostrum obcaecati rerum quos, atque nobis eaque
+          voluptatibus minima, at vitae optio animi odio esse possimus fugiat
+          explicabo reiciendis neque fuga cupiditate sunt voluptatem saepe.
+          Pariatur aut quaerat dolore numquam nostrum labore rem voluptatem
+          harum. Sint libero, vel illo ipsam nemo rerum. Fugiat tenetur,
+          accusantium possimus recusandae praesentium, a sed pariatur itaque
+          optio asperiores, vel accusamus repudiandae assumenda?
+        </p>
+        <h2 class="title-small">EXPERIENCE PROFESSIONNELLES</h2>
+        <!-- experience dev web -->
+        <div class="experience">
+          <div class="timeline">
+            <p>
+              2017 <br />
+              - <br />
+              2020
+            </p>
+          </div>
+          <div class="content">
+            <h3>Entrez le nom du poste</h3>
+            <p class="text-hint mb-2">le nom de l'entreprise</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+              assumenda. Assumenda eum quaerat rem? Quae iure commodi iste
+              aspernatur similique incidunt aperiam, veniam reiciendis quod
+              facilis perspiciatis molestiae. Mollitia, vero laborum dicta quam
+              nemo nisi facere accusamus ut quisquam quas consequuntur quo
+              ducimus velit repellendus labore! Laborum libero sunt iste.
+            </p>
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus,
+                quaerat (resumé d'expèrience).
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- experience cinema vfx-->
+        <div class="experience">
+          <div class="timeline">
+            <p>
+              2017 <br />
+              - <br />
+              2020
+            </p>
+          </div>
+          <div class="content">
+            <h3>Entrez le nom du poste</h3>
+            <p class="text-hint mb-2">le nom de l'entreprise</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+              assumenda. Assumenda eum quaerat rem? Quae iure commodi iste
+              aspernatur similique incidunt aperiam, veniam reiciendis quod
+              facilis perspiciatis molestiae. Mollitia, vero laborum dicta quam
+              nemo nisi facere accusamus ut quisquam quas consequuntur quo
+              ducimus velit repellendus labore! Laborum libero sunt iste.
+            </p>
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus,
+                quaerat (resumé d'expèrience).
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- experience game designer -->
+        <div class="experience">
+          <div class="timeline">
+            <p>
+              2017 <br />
+              - <br />
+              2020
+            </p>
+          </div>
+          <div class="content">
+            <h3>Entrez le nom du poste</h3>
+            <p class="text-hint mb-2">le nom de l'entreprise</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+              assumenda. Assumenda eum quaerat rem? Quae iure commodi iste
+              aspernatur similique incidunt aperiam, veniam reiciendis quod
+              facilis perspiciatis molestiae. Mollitia, vero laborum dicta quam
+              nemo nisi facere accusamus ut quisquam quas consequuntur quo
+              ducimus velit repellendus labore! Laborum libero sunt iste.
+            </p>
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus,
+                quaerat (resumé d'expèrience).
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
+      <!-- footer -->
+      <footer>
+        <h2 class="title-small">POUR VOUS RECONTACTER</h2>
+        <form method="post">
+          <i class="fas fa-envelope-open-text prefix"></i>
+          <input type="email" placeholder="Votre email" />
+          <button>
+            <a href="mailto:jcmairot@gmail.com"
+              ><i class="far fa-paper-plane postfix"></i
+            ></a>
+          </button>
+        </form>
+      </footer>
+    </div>
+
+    <!-- script -->
+    <script
+      src="https://kit.fontawesome.com/fae3248c47.js"
+      crossorigin="anonymous"
+    ></script>
+  </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -1,2 +1,1 @@
-// pour le changement de l'image id au survol
-document.querySelector("img.photoId");
+
